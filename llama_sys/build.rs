413a6c92a1841ba386f684a9cf7531aa044fc429
nytopop <ericizoita@gmail.com>
2024-06-12 09:34:18 -0700

    llama_sys: add more compile features

--- llama_sys/build.rs
+++ llama_sys/build.rs
@@ -14,39 +14,43 @@ fn main() {
         .define("LLAMA_BUILD_SERVER", "OFF")
         .very_verbose(true);
 
+    // TODO: add all the cmake opts... there's alot
+
     // cpu features
     let on = |x| if x { "ON" } else { "OFF" };
-    conf.define("LLAMA_AVX", on(cfg!(target_feature = "avx")));
-    conf.define("LLAMA_AVX2", on(cfg!(target_feature = "avx2")));
-    // TODO: avx512_*** and the cpu HBM stuff
-    conf.define("LLAMA_FMA", on(cfg!(target_feature = "fma")));
 
-    println!("cargo:rustc-link-lib=dylib=gomp");
+    conf.define("LLAMA_SVE", on(cfg!(feature = "sve")))
+        .define("LLAMA_AVX", on(cfg!(target_feature = "avx")))
+        .define("LLAMA_AVX2", on(cfg!(target_feature = "avx2")))
+        .define("LLAMA_AVX512", on(cfg!(target_feature = "avx512f")))
+        .define("LLAMA_AVX512_VBMI", on(cfg!(target_feature = "avx512vbmi")))
+        .define("LLAMA_AVX512_VNNI", on(cfg!(target_feature = "avx512vnni")))
+        .define("LLAMA_AVX512_BF16", on(cfg!(target_feature = "avx512bf16")))
+        .define("LLAMA_FMA", on(cfg!(target_feature = "fma")))
+        .define("LLAMA_F16C", on(cfg!(target_feature = "f16c")))
+        // backend stuff
+        .define("LLAMA_CPU_HBM", on(cfg!(feature = "cpu_hbm")))
+        .define("LLAMA_CUDA_FORCE_MMQ", on(cfg!(feature = "cuda_force_mmq")))
+        .define("LLAMA_CUDA", on(cfg!(feature = "cuda")))
+        .define("LLAMA_HIPBLAS", on(cfg!(feature = "rocm")))
+        .define("LLAMA_HIP_UMA", on(cfg!(feature = "rocm_uma")))
+        .define("LLAMA_VULKAN", on(cfg!(feature = "vulkan")))
+        .define("LLAMA_METAL", on(cfg!(feature = "metal")))
+        .define("LLAMA_KOMPUTE", on(cfg!(feature = "kompute")));
 
-    #[cfg(feature = "force_mmq")]
-    conf.define("LLAMA_CUDA_FORCE_MMQ", "ON");
-
-    // rocm (amdgpu)
-    #[cfg(feature = "rocm")]
-    {
-        conf.define("LLAMA_HIPBLAS", "ON");
+    if cfg!(feature = "rocm") {
         println!("cargo:rustc-link-search=/opt/rocm/lib");
         println!("cargo:rustc-link-lib=dylib=hipblas");
         println!("cargo:rustc-link-lib=dylib=amdhip64");
         println!("cargo:rustc-link-lib=dylib=rocblas");
     }
 
-    // cuda (nvidia)
-    #[cfg(feature = "cuda")]
-    {
-        conf.define("LLAMA_CUDA", "ON");
+    if cfg!(feature = "cuda") {
         println!("cargo:rustc-link-lib=dylib=cuda");
         println!("cargo:rustc-link-lib=dylib=cublas");
         println!("cargo:rustc-link-lib=dylib=cudart");
         println!("cargo:rustc-link-lib=dylib=cublasLt");
     }
-
-    // TODO: metal (apple silicon)
 
     let dst = conf.build();
 
@@ -55,6 +59,7 @@ fn main() {
     println!("cargo:rustc-link-search=native={}/build", dst.display());
     println!("cargo:rustc-link-lib=static=llama");
     println!("cargo:rustc-link-lib=stdc++");
+    println!("cargo:rustc-link-lib=dylib=gomp");
 
     let bind = bindgen::builder()
         .header("llama.cpp/llama.h")
